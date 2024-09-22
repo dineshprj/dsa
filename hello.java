75483303914101da4f85036793841dd7924a0ef6
dinesh prajapati <dineshprj7@gmail.com>
2024-09-22 18:59:01 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,186 @@
+// import java.util.Scanner;
+
+// public class hello {
+
+// // java program to read the number from standard input
+
+// /* public static void main (String args[]){
+// System.out.println("enter your numbers :");
+// Scanner sc= new Scanner(System.in);
+// int a=sc.nextInt();
+// System.out.println(a);*/
+
+// //java program to swape two number
+
+// /* public static void main(String args[]){
+// Scanner sc=new Scanner(System.in);
+// System.out.println("enter your number :");
+// System.out.print("a:");
+// int a=sc.nextInt();
+// System.out.print("b:");
+// int b=sc.nextInt();
+// int temp;
+// temp=a;
+// a=b;
+// b=temp;
+// System.out.println("swape numers are :\n a :"+a +"\n b: "+b);*/
+
+// // java program to add two binary strings
+
+// /* public static String addBinary(String a, String b) {
+// int num1 = Integer.parseInt(a, 2);
+// int num2 = Integer.parseInt(b, 2);
+
+// int sum = num1 + num2;
+
+// return Integer.toBinaryString(sum);
+// }
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
+// System.out.print("Enter the first binary number: ");
+// String binary1 = scanner.nextLine();
+
+// System.out.print("Enter the second binary number: ");
+// String binary2 = scanner.nextLine();
+// String result = addBinary(binary1, binary2);
+
+// System.out.println("Sum: " + result);
+// }
+// */
+
+// // write a program to reverse a given string without using built-in-function
+// /*public static void main(String args[]){
+// Scanner sc=new Scanner(System.in);
+// System.out.println("enter first string :");
+// String input=sc.nextLine();
+// String reversed="";
+// for(int i=input.length()-1;i>=0;i--){
+// reversed=reversed+input.charAt(i);
+// }
+// System.out.println("reversed String:"+reversed);
+// //Scanner.close();
+// }
+// */
+
+// // write a program to find a compound interest
+// //import java.util.Scanner;
+
+// //public class Hello {
+// /* public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+
+// System.out.print("Enter the principal amount: ");
+// float principal = sc.nextFloat();
+
+// System.out.print("Enter the annual interest rate (in %): ");
+// float rate = sc.nextFloat();
+
+// System.out.print("Enter the time (in years): ");
+// float time = sc.nextFloat();
+
+// // Calculate compound interest
+// float amount = principal * (1 + rate / 100) * time;
+
+// System.out.println("Compound Interest is: " + (amount - principal));
+
+// sc.close();
+// }*/
+// /* public static void main(String args[]){
+// Scanner sc=new Scanner(System.in);
+// System.out.print("enter your number: ");
+// int num=sc.nextInt();
+// for(int i=1;i<=num;i++){
+// for(int j=1;j<=num;j++){
+// System.out.print("*");
+// }
+// System.out.println();
+// }
+
+// }
+
+// }*/
+
+// // print star in java
+
+// /* public static void main(String args[]){
+// Scanner sc=new Scanner(System.in);
+// System.out.print("enter your number: ");
+// int num=sc.nextInt();
+// for(int i=1;i<=num;i++){
+// for(int j=1;j<=i;j++){
+// System.out.print("*");
+// }
+// System.out.println();
+// }
+
+// }
+// }
+
+// */
+// // Print Inverted Star in java
+// /* public static void main(String args[]){
+// Scanner sc=new Scanner(System.in);
+// System.out.print("enter your number: ");
+// int num=sc.nextInt();
+// for(int i=1;i<=num;i++){
+// for(int j=1;j<=num-i+1;j++)
+// {
+// // System.out.print(" ");
+// System.out.print("*");
+// }
+// System.out.println();
+// }
+
+// }
+// */
+// //print Half pyramid pattern
+
+// /* public static void main(String args[])
+// {
+// Scanner sc=new Scanner(System.in);
+// System.out.print("enter your number: ");
+// int num=sc.nextInt();
+// for(int i=1;i<=num;i++)
+// {
+// for(int j=1;j<=i;j++)
+// {
+// System.out.print(j);
+// }
+// System.out.println();
+// }
+
+// }
+// */
+// // print character pattern in java
+// /* public static void main(String args[]){
+// char ch='A';
+// Scanner sc=new Scanner(System.in);
+// System.out.print("enter your number: ");
+// int num=sc.nextInt();
+// for(int i=1;i<=num;i++){
+// for(int j=1;j<=i;j++){
+// System.out.print(ch);
+// ch++;
+// }
+// System.out.println();
+// }
+
+// }*/
+// // print a table
+// /*public static void main(String args[]){
+// Scanner sc =new Scanner(System.in);
+// System.out.println("enter your number : ");
+// int num=sc.nextInt();
+// System.out.println("table is :");
+// for(int i=1;i<=10;i++){
+// int sum=num*i;
+// System.out.println(num +" * "+ i +" = "+sum);
+
+// }
+// System.out.println();
+// }*/
+// public static void main(String args[]){
+
+// }
+
+// }
